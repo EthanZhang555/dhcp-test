ac992981f09f74a939e8fa8ae858e56e39d29730
Ethan <ethanzhang@qnap.com>
2025-08-28 11:10:27 +0800

    add dhcp filter and check option length

--- discover_request.c
+++ discover_request.c
@@ -100,7 +100,7 @@ unsigned short udp_checksum(const void *buff, unsigned short len, struct pseudo_
 }
 
 // ---------------- Build DHCP Discover / Request ----------------
-int build_dhcp_packet(uint8_t *packet, const uint8_t *src_mac, int is_request, uint32_t req_ip, uint32_t server_ip) {
+int build_dhcp_packet(uint8_t *packet, const uint8_t *src_mac, int is_request, uint32_t req_ip, uint32_t server_ip, uint32_t lease_time) {
     struct ethhdr *eth = (struct ethhdr *)packet;
     struct iphdr *ip = (struct iphdr *)(packet + ETHERNET_SIZE);
     struct udphdr *udp = (struct udphdr *)(packet + ETHERNET_SIZE + IP_SIZE);
@@ -152,10 +152,22 @@ int build_dhcp_packet(uint8_t *packet, const uint8_t *src_mac, int is_request, u
     dhcp->options[idx++] = (is_request ? 3 : 1); // 1 = Discover, 3 = Request
 
     // Requested IP
-    dhcp->options[idx++] = 50;
-    dhcp->options[idx++] = 4;
-    memcpy(&dhcp->options[idx], &req_ip, 4);
-    idx += 4;
+    if (req_ip != 0) {
+        dhcp->options[idx++] = 50;
+        dhcp->options[idx++] = 4;
+        memcpy(&dhcp->options[idx], &req_ip, 4);
+        idx += 4;
+    }
+
+    // Lease Time
+    if (lease_time != 0) {
+        uint32_t lease_net = htonl((uint32_t)lease_time);
+        dhcp->options[idx++] = 51;
+        dhcp->options[idx++] = 4;
+        memcpy(&dhcp->options[idx], &lease_net, 4);
+        idx += 4;
+    }
+    
 
     if (is_request) {
         
@@ -200,21 +212,30 @@ int build_dhcp_packet(uint8_t *packet, const uint8_t *src_mac, int is_request, u
     return ETHERNET_SIZE + IP_SIZE + UDP_SIZE + dhcp_len;
 }
 
-uint8_t *dhcp_get_option(uint8_t *options, int code) {
+uint8_t *dhcp_get_option(uint8_t *options, int code, int options_len) {
     uint8_t *opt = options;
     int index =0;
-    while (*opt != 255) {
+    while (index < options_len && *opt != 255) {
         if (*opt == 0) { opt++; continue; } // Pad
+        if (index + 2 > options_len){
+            printf("[error] Option header out of range\n");
+            return NULL;
+        }
         uint8_t opt_code = opt[0];
         uint8_t len = opt[1];
+        if (index + 2 + len > options_len) {
+            printf("[error] Option too long\n");
+            return NULL;
+        }
         if (opt_code == code) {
+            printf("[info] found\n");
             return &opt[2]; // 回傳 data 開頭
         }
-        // printf("opt_code:%d, len:%d\n",opt_code,len);
+        
         opt += 2 + len;
         index += 2 + len;
     }
-    printf("index:%d\n",index);
+    printf("[info] not found\n");
     return NULL; // not found
 }
 
@@ -245,10 +266,11 @@ int main() {
     uint8_t src_mac[6] = {0x00, 0xe0, 0x4c, 0x68, 0x03, 0x53};
     uint8_t packet[1500];
 
-    uint32_t req_ip = inet_addr("10.35.72.12");
+    uint32_t req_ip = inet_addr("10.35.72.2");
+    uint32_t lease_time = 172800; // 2天
 
     // 發送 Discover
-    int pkt_len = build_dhcp_packet(packet, src_mac, 0, req_ip, 0);
+    int pkt_len = build_dhcp_packet(packet, src_mac, 0, req_ip, 0 ,lease_time);
     if (pcap_sendpacket(fp, packet, pkt_len) != 0) {
         printf("Error sending DHCP Discover\n");
     } else {
@@ -258,14 +280,51 @@ int main() {
     //確認是否是discover和request之間間隔過常導致server沒發送ack
     // Sleep(3000);
 
+    struct bpf_program fcode;
+    if (pcap_compile(fp, &fcode, "udp and (port 67 or port 68)", 1, 0xffffffff) < 0) {
+        printf("Unable to compile the packet filter.\n");
+        pcap_close(fp);
+        return -1;
+    }
+    if (pcap_setfilter(fp, &fcode) < 0) {
+        printf("Error setting the filter.\n");
+        pcap_close(fp);
+        return -1;
+    }
+
     while (1) {
         struct pcap_pkthdr *header;
         const u_char *pkt_data;
         int res = pcap_next_ex(fp, &header, &pkt_data);
         if (res == 1) {
+            printf("receive packet\n");
             // TODO: 在這裡解析 DHCP Offer
-            struct dhcp_packet *dhcp = (struct dhcp_packet *)(pkt_data + ETHERNET_SIZE + IP_SIZE + UDP_SIZE);
-            uint8_t *messageType = dhcp_get_option(dhcp->options+4,53);
+            // 先取 IP header
+            struct iphdr *ip = (struct iphdr *)(pkt_data + ETHERNET_SIZE);
+            int ip_header_len = ip->ihl * 4;
+            // 取 UDP header
+            struct udphdr *udp = (struct udphdr *)(pkt_data + ETHERNET_SIZE + ip_header_len);
+
+            // UDP payload 長度 (含 DHCP 全部資料)
+            int udp_payload_len = ntohs(udp->len) - sizeof(struct udphdr);
+
+            // DHCP 固定長度 = 236 bytes + 4 bytes magic cookie = 240
+            int dhcp_fixed_len = 240;
+
+            if (udp_payload_len <= dhcp_fixed_len) {
+                printf("錯誤: DHCP payload 太短 (len=%d)\n", udp_payload_len);
+                continue;
+            }
+            // DHCP 封包起始
+            struct dhcp_packet *dhcp = (struct dhcp_packet *)((uint8_t *)udp + sizeof(struct udphdr));
+
+            // Options 起始位置
+            uint8_t *options = dhcp->options+4;//排除magic code
+
+            // Options 的實際長度
+            int options_len = udp_payload_len - dhcp_fixed_len;
+            printf("options_len:%d\n",options_len);
+            uint8_t *messageType = dhcp_get_option(options,53,options_len);
             if (2 == (int) messageType[0]) {
                 uint8_t *ip = (uint8_t *)&dhcp->yiaddr;
                 printf("yiaddr = %d.%d.%d.%d\n", ip[0], ip[1], ip[2], ip[3]);
@@ -279,7 +338,7 @@ int main() {
     uint32_t server_ip = inet_addr("10.35.72.1");
 
     // 發送 Request
-    pkt_len = build_dhcp_packet(packet, src_mac, 1, req_ip, server_ip);
+    pkt_len = build_dhcp_packet(packet, src_mac, 1, req_ip, server_ip,0);
     if (pcap_sendpacket(fp, packet, pkt_len) != 0) {
         printf("Error sending DHCP Request\n");
     } else {
